4bc84927ccfead53c14311623139c13806f5c8bc
Bluetouch <bogusz28@wp.pl>
2020-08-18 17:51:33 +0200

    Initial commit

---
+++ src/test/java/pl/application/jobSearchEngine/JobSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.application.jobSearchEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
